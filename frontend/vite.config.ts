7ae30f94ec62152bc7a89e9d3450f3d05a4de4c3
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-09-02 17:47:09 +0300

    Update frontend package and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/team-project-culinary-platform/",
   plugins: [react()],
   optimizeDeps: {
     // исключаем пакеты с нативными модулями, которые ломаются на этапе оптимизации
