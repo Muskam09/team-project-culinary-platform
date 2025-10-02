7175a1af8b3012756c33ffda6b7ea91d073f73d4
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-10-02 13:41:52 +0300

    FE-bootstrap: update vite.config and fix settings page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/team-project-culinary-platform/",
+  base: "/",
   plugins: [react(), svgr()],
   optimizeDeps: {
     exclude: ['lightningcss'],
